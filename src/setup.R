c63e3e08c137e91e3a7e5ab985536d3655ab1d7c
Andreas Bauer <dev@andreasbauer.org>
2023-06-27 16:00:15 +0200

    Add dependencies for BDA in setup.R

--- src/setup.R
+++ src/setup.R
@@ -3,3 +3,11 @@ library(devtools)
 
 devtools::install_github('rstudio/rmarkdown')
 install.packages("tidyverse")
+
+# For BRMS
+install.packages("rstan")
+install.packages("brms")
+
+install.packages("ggdag")
+install.packages("marginaleffects")
+
